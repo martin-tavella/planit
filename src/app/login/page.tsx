44efa19a6bedb91b1d5ac5d92759f4108ee955e9
martin-tavella <martutavella@gmail.com>
2025-08-01 16:42:30 -0300

    feat: metadata in login and register

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,10 @@
 import Login from "@/components/Login";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Planit - Login",
+    description: "Login to your account",
+}
 
 export default function LoginPage() {
     return <Login />
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,13 @@
 import Register from "@/components/Register";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Planit - Register",
+    description: "Create your account",
+}
+
+
 export default function RegisterPage() {
     return <Register />
 }
